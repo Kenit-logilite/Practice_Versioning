a007159ada74521c35e5a81bedc68555916bf5ba
Kenit <kenit.patel@logilite.com>
2021-06-06 15:57:42 +0530

    Bean Class Created

---
+++ src/Emp.java
@@ -0,0 +1,55 @@
+public class Emp {
+	private int id;
+	private String name,gender, password, country,phone; 
+	
+	public int getid() {
+		return id;
+	}
+	public void setid(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	
+	
+	public String getphone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
